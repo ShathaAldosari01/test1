ae2baa0d6579ad08ae30bed986e1f3b0bbcc7774
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-04 10:29:00 +0300

    simple program: when you full the form and click registr your info will display bellow

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -17,10 +17,39 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void displayNamebtn(View view){
-        TextView helloTxt = findViewById(R.id.helloTxt);
-        EditText name = findViewById(R.id.name);
-        helloTxt.setText("Hello "+name.getText().toString());
-    }//end displayNamebtn()
+//    public void displayInfo(View view){
+//        TextView helloTxt = findViewById(R.id.helloTxt);
+//        EditText name = findViewById(R.id.name);
+//        helloTxt.setText("Hello "+name.getText().toString());
+//    }//end displayNamebtn()
+
+    public void register(View view){
+        //get info
+        EditText fNameIn = findViewById(R.id.firstNameInput),
+                 lNameIn = findViewById(R.id.lastNameInput),
+                 emailIn = findViewById(R.id.emailInput);
+
+        //get where to display
+        TextView fNameOut = findViewById(R.id.firstNameTxt),
+                 lNameOut = findViewById(R.id.lastNameTxt),
+                 emailOut = findViewById(R.id.emailTxt);
+
+        //display info
+        fNameOut.setText("First Name: "+fNameIn.getText().toString());
+        lNameOut.setText("Last Name: "+ lNameIn.getText().toString());
+        emailOut.setText("Email: "+ emailIn.getText().toString());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }//end class
