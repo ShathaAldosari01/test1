d97189927a3177a0adf91a75c60fd0603d7cfdb9
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-04 05:49:40 +0300

    simple program when you click the btn it say hello follow by your name

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.test1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
-}
+    public void displayNamebtn(View view){
+        TextView helloTxt = findViewById(R.id.helloTxt);
+        EditText name = findViewById(R.id.name);
+        helloTxt.setText("Hello "+name.getText().toString());
+    }//end displayNamebtn()
+
+}//end class
